2abeae5881b69e36905cfc9a6278146afa0f669c
zaadevofc <zaadevofc@gmail.com>
2024-05-30 05:12:15 +0700

    nothing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://zaadevofc.github.io/gabud",
   base: "/gabud",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), tailwind()]
 });
